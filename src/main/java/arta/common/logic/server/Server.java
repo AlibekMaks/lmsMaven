98b76188aa176253d62549426ca5d6cf3caefaca
Mukha <Mukha>
2015-09-18 09:56:50 +0000

    lms problema  s hostom

--- src/main/java/arta/common/logic/server/Server.java
+++ src/main/java/arta/common/logic/server/Server.java
@@ -2,6 +2,7 @@ package arta.common.logic.server;
 
 import arta.common.Constants;
 import arta.common.http.MimeTypes;
+import arta.common.logic.util.Log;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.ServletContext;
@@ -32,19 +33,25 @@ public class Server {
             MimeTypes.init(servletContext);
             synchronized(syncObject){
                 try{
+
                     URL url = new URL(request.getRequestURL().toString());
                     isURLCorrect = url.getProtocol().toLowerCase().equals("http");
-
                     if (!isURLCorrect){
                         MAIN_URL = "";
                         return;
                     }
+                    MAIN_URL = "" + path;
 
-                    MAIN_URL = url.getProtocol()+"://"+url.getHost() ;
-                    if(url.getPort() != -1){
-                        MAIN_URL += ":"+url.getPort();
-                    }
-                    MAIN_URL +=  path;
+
+//
+//                    MAIN_URL = url.getProtocol()+"://"+url.getHost() ;
+//                    Log.writeLog("step3 " + MAIN_URL);
+//                    Log.writeLog("step4 " + path);
+//                    if(url.getPort() != -1){
+//                        MAIN_URL += ":"+url.getPort();
+//                        Log.writeLog("step5 " + MAIN_URL);
+//                    }
+//                    MAIN_URL +=  path;
                     WAR_NAME = path;
                 }catch(Exception e){
                     MAIN_URL = "";
